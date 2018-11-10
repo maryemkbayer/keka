f980b894696ce5a8be158a82bae18f7529eb08ab
maryemkbayer <44728191+maryemkbayer@users.noreply.github.com>
2018-11-10 16:53:11 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+var name=prompt("ahla bike")
+var a=confirm("jayy")
+document.querySelector(".haja").style.backgroundColor ="yellow";
+document.querySelector(".haja").style.fontSize="50px";
+
+var button = document.getElementById('test');
+var para = document.getElementById('para');
+
+button.addEventListener('click', function(){
+    para.style.backgroundColor="yellow";
+})
+document.getElementById("img").setAttribute("src","res/image.jpg")
+document.getElementById("self").innerHTML="i am here"
+function yo(){
+    alert('yo');
+}
+setTimeout(yo,2000);
+setInterval(yo,3000)
